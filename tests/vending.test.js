dc4ae4e8c2c08b269a2c6ab895559c102157ed80
Zagir <snuuymoou@gmail.com>
2026-04-06 10:58:22 +0500

    Initial commit

---
+++ tests/vending.test.js
@@ -0,0 +1,35 @@
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("VendingMachine", function () {
+    it("test payable method", async function () {
+        const VendingMachine = await ethers.getContractFactory("VendingMachine");
+        const vending = await VendingMachine.deploy("VirtualCupcakes");
+        await vending.deployed();
+        console.log("VendingMachine deployed at:" + vending.address);
+        const [owner, acc_1] = await ethers.getSigners();
+        
+        expect((await vending.getVendingMachineBalance()).toNumber()).to.equal(100);
+        
+        await vending.connect(acc_1).purchase(1, {value: 1e9});
+        
+        // ИСПРАВЛЕНО: добавляем .toNumber() здесь
+        expect((await vending.getVendingMachineBalance()).toNumber()).to.equal(99);
+    });
+
+    it("test refill", async function () {
+        const VendingMachine = await ethers.getContractFactory("VendingMachine");
+        const vending = await VendingMachine.deploy("VirtualCupcakes");
+        await vending.deployed();
+        console.log("VendingMachine deployed at:" + vending.address);
+        const [owner, acc_1] = await ethers.getSigners();
+        
+        expect((await vending.getVendingMachineBalance()).toNumber()).to.equal(100);
+        
+        await vending.connect(acc_1).purchase(1, {value: 1e9});
+        expect((await vending.getVendingMachineBalance()).toNumber()).to.equal(99);
+        
+        await vending.refill(1);
+        expect((await vending.getVendingMachineBalance()).toNumber()).to.equal(100);
+    });
+});
